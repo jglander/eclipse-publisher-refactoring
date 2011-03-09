6810e147d043a54c91cdfff8b90403f98ec85f53
Tobias Oberlies <tobias.oberlies@sap.com>
2011-03-09 14:29:21 +0100

    Added missing copyright notice

--- org.eclipse.pde.publishing/src/org/eclipse/pde/internal/publishing/Activator.java
+++ org.eclipse.pde.publishing/src/org/eclipse/pde/internal/publishing/Activator.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SAP AG - consolidation of publishers for PDE formats
+ *******************************************************************************/
 package org.eclipse.pde.internal.publishing;
 
 import org.osgi.framework.BundleActivator;
